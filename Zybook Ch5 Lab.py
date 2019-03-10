078e71dd941b7da82dc14377b04529e875b573f6
eqomi <47514207+eqomi@users.noreply.github.com>
2019-03-10 19:37:14 -0400

    Zybook chapter 5 Lab: Create Password
    this is the code I wrote for the lab in Chapter 5 of Zybook

---
+++ Zybook Ch5 Lab.py
@@ -0,0 +1,19 @@
+# FIXME (1): Finish reading another word and an integer into variables. 
+# Output all the values on a single line
+favoriteColor = input('Enter favorite color: \n')
+favoritePet = input('Enter pet\'s name: \n')
+userNum = int(input('Enter a number: \n'))
+print('You entered:',favoriteColor, favoritePet, userNum)
+
+# FIXME (2): Output two password options
+password1 = favoriteColor + '_' + favoritePet
+password2 = str(userNum) + favoriteColor + str(userNum)
+print('\nFirst password:', password1)
+print('Second password:', password2)
+
+
+# FIXME (3): Output the length of the two password options
+password1Len = len(password1)
+password2Len = len(password2)
+print('\nNumber of characters in %s: %d' % (password1,password1Len))
+print('\nNumber of characters in %s: %d' % (password2,password2Len))
